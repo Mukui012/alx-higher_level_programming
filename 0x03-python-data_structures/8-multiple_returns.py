58b4e124fe6f823db7fae5ad2325d90010551c39
Mukui012 <spencermukui@gmail.com>
2023-06-12 21:45:25 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-        length = len(sentence)
-        return (length, sentence[0] if length > 0 else None)
+    length = len(sentence)
+    return (length, sentence[0] if length > 0 else None)
