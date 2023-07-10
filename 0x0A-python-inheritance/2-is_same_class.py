8ebe4fd2b95236a2840b43f6b9f80a17a1c4b781
Mukui012 <spencermukui@gmail.com>
2023-07-10 20:49:13 +0000

    function that returns a bool value if an object is an instance of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,7 +6,4 @@ of a class"""
 
 def is_same_class(obj, a_class):
     """Definition of module is_same_class"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
