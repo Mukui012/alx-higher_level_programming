e8264350ef450febdbdcb3d97ccd9e2367e2c0a9
Mukui012 <spencermukui@gmail.com>
2023-07-10 21:26:56 +0000

    class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bn/python3
+""" class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ Definition with exception message"""
+    def area(self):
+        raise Exception("area() is not implemented")
