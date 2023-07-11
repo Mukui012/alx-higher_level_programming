aa9b04c0d37ff4f8257e7372609c31b7ab0d03aa
Mukui012 <spencermukui@gmail.com>
2023-07-11 15:54:56 +0000

    function that writes to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that writes a string to a text file
+and returns the number of caracters written"""
+
+
+def write_file(filename="", text=""):
+    """Prints string to text file"""
+    with open(filename, mode='w', encoding="utf-8") as file:
+        file.write()
