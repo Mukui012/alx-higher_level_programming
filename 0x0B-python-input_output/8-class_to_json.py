593b41a1806e9073964e59cd4b2e9a11ff5c732a
Mukui012 <spencermukui@gmail.com>
2023-07-11 19:59:14 +0000

    returns dictionary representation of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,5 +4,5 @@ simple data structure for JSON serialization of an object"""
 
 
 def class_to_json(obj):
-    """Returns the dictionary representaton"""
+    """Returns the dictionary representation of an object"""
     return obj.__dict__
