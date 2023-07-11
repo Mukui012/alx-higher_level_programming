d8b76122be7e5679996e4472f3b4efdb36e85f9d
Mukui012 <spencermukui@gmail.com>
2023-07-11 20:31:34 +0000

    class Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,25 +1,23 @@
-#!usr/bin/python3
-"""Module that defines class Student"""
+#!/usr/bin/python3
+""" Module that defines a class Student"""
 
 
 class Student:
+    """Represent class Student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes instance of the class"""
+        """Initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """Returns dictionary representation"""
-        my_dict = dict()
-        if type(attrs) is list and all(type(i is str for i in attrs):
-            for i in attrs:
-                if i in self.__dict__:
-                    my_dict.update({i: self.__dict__[i]})
-            return my_dict
-        return self.__dict__.copy()
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
         """Replaces all attributes of Student"""
-        for x in json:
-            self.__dict__.update({x: json[x]})
+        for a, b in json.items():
+            setattr(self, a, b)
