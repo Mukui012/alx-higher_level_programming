fcef56bb18082c6e10405e2d39ec4cbe5e324eda
Mukui012 <spencermukui@gmail.com>
2023-07-11 20:13:57 +0000

    class Student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that defines class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializes instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation"""
+        my_dict = dict()
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            for i in attrs:
+                if i in self.__dict__:
+                    my_dict.update({i: self.__dict__[i]})
+            return my_dict
+        return self.__dict__.copy()
