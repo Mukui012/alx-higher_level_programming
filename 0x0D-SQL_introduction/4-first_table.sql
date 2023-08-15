9cac6ff84f3c7b50f7dba3cf0bee042f85139204
Mukui012 <spencermukui@gmail.com>
2023-08-15 20:45:53 +0000

    script that creates a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- script that creates table in current database
 -- script won't fail if the table already exists
 
-CREATE TABLE IF NOT EXISTS first_table (id(INT), name(VARCHAR(256)));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name(VARCHAR(256));
