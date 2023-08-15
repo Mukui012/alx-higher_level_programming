ddb9e5ed1ba6370d222261a059a5152450972d7e
Mukui012 <spencermukui@gmail.com>
2023-08-15 21:25:27 +0000

    script that lists all records of table second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- script that displays all records of second_table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC;
