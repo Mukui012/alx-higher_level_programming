eab1b4b1cc62e392b510c406a0545ef82947a884
Mukui012 <spencermukui@gmail.com>
2023-09-21 20:26:20 +0000

    script that adds new state to the database

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,21 +10,20 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-	""" Acces the database """
-	username = argv[1]
-	password = argv[2]
-	db = argv[3]
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-			format(username, password, db), pool_pre_ping=True)
-	Base.metadata.create_all(engine)
-	Session = sessionmaker(bind=engine)
-	session = Session()
+    """ Acces the database """
+    username = argv[1]
+    password = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-	""" Add new state """
-	new_state = State(name="Louisiana")
-	session.add(new_state)
-	session.commit()
+    """ Add new state """
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
 
-	print("{:d}".format(new.id))
-	
-	session.close()
+    print("{:d}".format(new_state.id))
+    session.close()
