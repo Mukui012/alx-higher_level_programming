027d7f4eef797b4ba0dbdd6118aa703847094678
Mukui012 <spencermukui@gmail.com>
2023-09-29 20:45:18 +0000

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 import urllib.request
 
 if __name__ == '__main__':
-    request=urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    request = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(request) as response:
         page = response.read()
         page_decoded = page.decode('utf-8')
